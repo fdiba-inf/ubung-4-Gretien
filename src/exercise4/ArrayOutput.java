d019714c1cc31f2062a7a8ed15771c286a128011
Gretien <gretien.kolev@fdiba-tu.sofia.bg>
2020-10-24 11:54:55 +0000

    a

---
+++ src/exercise4/ArrayOutput.java
@@ -0,0 +1,27 @@
+package exercise4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOutput {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String p = "";
+        int m = input.nextInt();
+
+        int[] numbers = new int[m];
+
+        System.out.println("Enter numbers: ");
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = input.nextInt();
+        }
+        for (int index = 0; index < numbers.length; index++){
+        System.out.println("Numbers: " + p + numbers[index]);
+        p = p + " ";
+        }
+
+        
+    }
+
+}
